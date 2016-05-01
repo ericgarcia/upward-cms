110be1eea2a02f99521a5099cce1b7b89e465c80
Eric Garcia <eric.garcia@gmail.com>
2016-05-01 15:34:24 -0400

    simple view for showing articles

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,15 +1,36 @@
 var keystone = require('keystone');
 
 exports = module.exports = function(req, res) {
-	
+
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
-	
+
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
-	
-	// Render the view
+  locals.data = {
+		articles: []
+	};
+  locals.protocol = req.protocol;
+
+	// Load the Articles
+	view.on('init', function(next) {
+    console.log("query articles")
+
+		var Article = keystone.list('Article');
+
+		Article.model.find()
+			.limit(3)
+			.where('state', 'published')
+			// .populate('owner')
+			.exec(function(err, results) {
+				locals.data.articles = results;
+				next(err);
+			});
+
+	});
+
+  // Render the view
 	view.render('index');
-	
+
 };
