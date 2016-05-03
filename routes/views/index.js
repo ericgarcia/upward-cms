1ddb0da5999a44388eda87fd18936e99f45bfa3c
Eric Garcia <eric.garcia@gmail.com>
2016-05-03 18:36:54 -0400

    untabify

--- routes/views/index.js
+++ routes/views/index.js
@@ -2,37 +2,37 @@ var keystone = require('keystone');
 
 exports = module.exports = function(req, res) {
 
-	var view = new keystone.View(req, res);
-	var locals = res.locals;
+  var view = new keystone.View(req, res);
+  var locals = res.locals;
 
-	// locals.section is used to set the currently selected
-	// item in the header navigation.
-	locals.section = 'home';
+  // locals.section is used to set the currently selected
+  // item in the header navigation.
+  locals.section = 'home';
   locals.data = {
-		articles: []
-	};
+    articles: []
+  };
   locals.protocol = req.protocol;
 
-	// Load the Articles
-	view.on('init', function(next) {
+  // Load the Articles
+  view.on('init', function(next) {
     console.log("query articles")
 
-		var Article = keystone.list('Article');
+    var Article = keystone.list('Article');
 
-		Article.model.find()
-			.limit(3)
-			.where('state', 'published')
-			// .populate('owner')
+    Article.model.find()
+      .limit(3)
+      .where('state', 'published')
+      // .populate('owner')
       .populate('subjects')
       .populate('contentTypes')
-			.exec(function(err, results) {
-				locals.data.articles = results;
-				next(err);
-			});
+      .exec(function(err, results) {
+        locals.data.articles = results;
+        next(err);
+      });
 
-	});
+  });
 
   // Render the view
-	view.render('index');
+  view.render('index');
 
 };
